c09d7ac93508ca499d4bb1cbbccd4516107f707a
Yann Leost <yannleost@gmail.com>
2018-06-10 10:47:58 +0200

    model almost done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_084307) do
+ActiveRecord::Schema.define(version: 2018_06_10_084738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2018_06_10_084307) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.integer "places_left"
+    t.date "date"
+    t.bigint "flat_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_reservations_on_flat_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "lastname"
@@ -35,4 +46,6 @@ ActiveRecord::Schema.define(version: 2018_06_10_084307) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "flats"
+  add_foreign_key "reservations", "users"
 end
